82b149525cabd9a4c7565deed3ee8deca3dc3a81
yağmur başgöl <20070002005@stu.yasar.edu.tr>
2025-04-23 20:40:45 +0300

    Initial commit

--- app/config.py
+++ app/config.py
@@ -1,13 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql://flights_db_jxxi_user:d7LAp9KQqBh86MXSlJuec1yv2BA1tQAk@dpg-d03burruibrs73838mfg-a.frankfurt-postgres.render.com/flights_db_jxxi"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
-    SWAGGER = {
-        'title': 'Airline Ticketing API',
-        'uiversion': 3
-    }
